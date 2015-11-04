302cd43155d1f7905f509ef935aa265e35178e01
jessy100 <jessyvisch@gmail.com>
2015-11-04 20:49:02 +0100

    fix various camera issues

--- Ejento 148/Camera.cpp
+++ Ejento 148/Camera.cpp
@@ -2,20 +2,28 @@
 #include "Camera.h"
 
 
-Camera::Camera(Player & player) :
-	player{ player }
+Camera::Camera(Player & player, int levelWidth) :
+	player{ player },
+	levelWidth{levelWidth}
 {
 	screenDimensions = sf::Vector2f(896, 640);
 	view.reset(sf::FloatRect(0, 0, screenDimensions.x, screenDimensions.y));
 	view.setViewport(sf::FloatRect(0, 0, 1.0f, 1.0f));
 }
 
+void Camera::Reset(sf::RenderWindow &window) {
+	view.reset(sf::FloatRect(0,0, screenDimensions.x, screenDimensions.y));
+	window.setView(view);
+}
 
 void Camera::update(sf::RenderWindow &window) {
 	cameraPosition.x = player.getPosition().x - (screenDimensions.x / 2);
 
 	if (cameraPosition.x < 0) {
 		cameraPosition.x = 0;
+	}
+	if (cameraPosition.x > (levelWidth - screenDimensions.x)) {
+		cameraPosition.x = levelWidth - screenDimensions.x;
 	}
 	view.reset(sf::FloatRect(cameraPosition.x, cameraPosition.y, screenDimensions.x, screenDimensions.y));
 	window.setView(view);
--- Ejento 148/Camera.h
+++ Ejento 148/Camera.h
@@ -1,10 +1,13 @@
+#ifndef _Camera_HPP
+#define _Camera_HPP
+
 #include "player.h"
 
 class Camera
 {
 public:
-	Camera(Player & player);
-
+	Camera(Player & player, int levelWidth);
+	void Reset(sf::RenderWindow &window);
 	void update(sf::RenderWindow &window);
 	sf::Vector2f getPosition() { return cameraPosition; }
 
@@ -13,4 +16,7 @@ private:
 	Player & player;
 	sf::Vector2f screenDimensions;
 	sf::Vector2f cameraPosition;
+	int levelWidth;
 };
+
+#endif
